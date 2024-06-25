ec50c3ee649a26bedee2b1e9451f4eaf5e2844cb
youzusong <youzusong@outlook.com>
2024-06-25 23:05:42 +0800

    IdGenerator

--- Framework/You.Archi.IdGenerator.Redis/RedisIdGenerator.cs
+++ Framework/You.Archi.IdGenerator.Redis/RedisIdGenerator.cs
@@ -1,23 +1,61 @@
-﻿namespace You.Archi.IdGenerator.Redis
-{
-    public class RedisIdGenerator
-    {
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using StackExchange.Redis;
+using System.Reflection;
 
-        public long NewId(string keyPrefix)
+namespace You.Archi.IdGenerator.Redis
+{
+    /// <summary>
+    /// Redis之Id生成器
+    /// </summary>
+    public class RedisIdGenerator : IDisposable
+    {
+        private const long EPOCH_TIME = 63854236800L;   // 初始时间：2024-06-18
+        private const int SCQUENCE_BIT = 32;            // 序列号长度
+
+        private static readonly MethodInfo? _RedisConnectMethod;    // Redis连接方法
+        private static readonly MethodInfo? _RedisDisposeMethod;    // Redis释放方法
+
+        private readonly IDistributedCache _cache;
+
+        static RedisIdGenerator()
+        {
+            // 反射获取RedisCache的内部方法
+            var type = typeof(RedisCache);
+            _RedisConnectMethod = type.GetMethod("Connect", BindingFlags.Instance | BindingFlags.NonPublic);
+            _RedisDisposeMethod = type.GetMethod("Dispose", BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        public RedisIdGenerator(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        /// <summary>
+        /// 生成新Id
+        /// </summary>
+        /// <param name="sceneKey">业务场景Key</param>
+        /// <returns>新Id</returns>
+        public long NewId(string sceneKey)
         {
             var dtNow = DateTime.Now;
 
-            // 获取时间戳
-            var timestamp = 0L;
+            // 1、获取时间戳(秒)
+            var timestamp = dtNow.ToUniversalTime().Ticks / 10000000 - EPOCH_TIME;
 
+            // 2、获取序列号
+            var db = (IDatabase)_RedisConnectMethod?.Invoke(_cache, null)!;
+            var key = $"incr:{sceneKey}:{dtNow.ToString("yyyyMMdd")}";
+            var scquence = db.StringIncrement(key);
 
-            // 获取序列号
-            var key = $"incr:{keyPrefix}:{dtNow.ToString("yyyyMMdd")}";
-            var count = 0L;
-
-            // 拼接：空位(1bit) + 时间戳(31bit) + 序列号(32bit)
-            var result = timestamp << 32 | count;
+            // 3、拼接：空位(1bit) + 时间戳(31bit) + 序列号(32bit)
+            var result = timestamp << SCQUENCE_BIT | scquence;
             return result;
+        }
+
+        public void Dispose()
+        {
+            _RedisDisposeMethod?.Invoke(_cache, null);
         }
 
     }
--- Framework/You.Archi.IdGenerator.Redis/obj/Debug/net8.0/You.Archi.IdGenerator.Redis.AssemblyInfo.cs
+++ Framework/You.Archi.IdGenerator.Redis/obj/Debug/net8.0/You.Archi.IdGenerator.Redis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("You.Archi.IdGenerator.Redis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6612db3077e9ca41fdb7026b9d4b713050f0135")]
 [assembly: System.Reflection.AssemblyProductAttribute("You.Archi.IdGenerator.Redis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("You.Archi.IdGenerator.Redis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
