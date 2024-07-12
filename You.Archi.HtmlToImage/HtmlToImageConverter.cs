127f6283d79e0740e0b807754413f5e30a6b05c2
youzusong <youzusong@outlook.com>
2024-07-12 16:13:42 +0800

    HtmlToImage

---
+++ You.Archi.HtmlToImage/HtmlToImageConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace You.Archi.HtmlToImage
+{
+    public class HtmlToImageConverter
+    {
+        private static readonly string _toolFileName = "wkhtmltoimage.exe";
+
+        private readonly string _toolFilePath;
+
+        public HtmlToImageConverter(string toolFilePath)
+        {
+            _toolFilePath = toolFilePath;
+        }
+
+
+
+    }
+}
